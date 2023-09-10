bd476002699b8cba36e8164491997c7e609eb470
taufik69 <taufik35-2915@diu.edu.bd>
2023-09-10 16:45:14 +0600

    dom 2 done

--- dom-2/js/main.js
+++ dom-2/js/main.js
@@ -8,11 +8,20 @@ function main() {
   const root = document.querySelector(".root");
   const btn = document.querySelector(".btn");
   const output = document.querySelector(".output");
+  const copyBtn = document.querySelector(".copyBtn");
+  const copy_output = document.querySelector(".copy_output");
 
   //   ---> Now click button and generate random color and hexa code
   btn.addEventListener("click", function () {
     root.style.backgroundColor = GenerateHexacolor();
     output.value = GenerateHexacolor();
+    copy_output.innerHTML = "";
+  });
+  copyBtn.addEventListener("click", function () {
+    navigator.clipboard.writeText(output.value);
+    copy_output.innerHTML = `Copy sucssfull ${output.value}`;
+    copy_output.style.color = "green";
+    copy_output.style.textShadow = "white";
   });
 }
 
